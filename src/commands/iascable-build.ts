fa4d34d60246d4f187f70fb0b87df69a8cdfea3f
Sean Sundberg <seansund@us.ibm.com>
2022-02-27 23:25:14 -0600

    Fixes launch script generation in output directory
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/commands/iascable-build.ts
+++ src/commands/iascable-build.ts
@@ -1,6 +1,6 @@
 import {Container} from 'typescript-ioc';
 import {Arguments, Argv} from 'yargs';
-import {promises} from 'fs';
+import {promises, fchmod} from 'fs';
 import {default as jsYaml} from 'js-yaml';
 import {dirname, join} from 'path';
 
@@ -165,21 +165,21 @@ async function outputTile(rootPath: string, tile: Tile | undefined) {
 }
 
 async function outputLaunchScript(rootPath: string) {
-  await promises.mkdir(rootPath, {recursive: true})
+  const launchScriptPath: string = join(rootPath, '../launch.sh')
 
-  return promises.copyFile(join(getScriptsPath(), 'launch.sh'), join(rootPath, '../launch.sh'))
-    .catch(() => {
-      console.log('  Error writing launch script')
+  await promises.writeFile(launchScriptPath, launchScript)
+    .catch(err => {
+      console.log('  Error writing launch script', err)
       return {}
     })
-}
 
-function getScriptsPath(): string {
-  if (new RegExp('dist/').test(__dirname)) {
-    return join(__dirname, '../../../scripts')
+  try {
+    const { fd } = await promises.open(launchScriptPath, 'r');
+    fchmod(fd, 0o777, err => {
+      if (err) throw err;
+    });
+  } catch (error) {
   }
-
-  return join(__dirname, '../../scripts')
 }
 
 async function outputResult(rootPath: string, result: IascableResult): Promise<void> {
@@ -188,3 +188,42 @@ async function outputResult(rootPath: string, result: IascableResult): Promise<v
   await outputTile(rootPath, result.tile);
   await outputLaunchScript(rootPath);
 }
+
+const launchScript: string = `#!/bin/bash
+
+# IBM Ecosystem Labs
+
+SCRIPT_DIR="$(cd $(dirname $0); pwd -P)"
+SRC_DIR="\${SCRIPT_DIR}"
+
+DOCKER_IMAGE="quay.io/ibmgaragecloud/cli-tools:v1.1"
+
+SUFFIX=$(echo $(basename \${SCRIPT_DIR}) | base64 | sed -E "s/[^a-zA-Z0-9_.-]//g" | sed -E "s/.*(.{5})/\\1/g")
+CONTAINER_NAME="cli-tools-\${SUFFIX}"
+
+echo "Cleaning up old container: \${CONTAINER_NAME}"
+
+DOCKER_CMD="docker"
+\${DOCKER_CMD} kill \${CONTAINER_NAME} 1> /dev/null 2> /dev/null
+\${DOCKER_CMD} rm \${CONTAINER_NAME} 1> /dev/null 2> /dev/null
+
+if [[ -n "$1" ]]; then
+    echo "Pulling container image: \${DOCKER_IMAGE}"
+    \${DOCKER_CMD} pull "\${DOCKER_IMAGE}"
+fi
+
+ENV_FILE=""
+if [[ -f "credentials.properties" ]]; then
+  ENV_FILE="--env-file credentials.properties"
+fi
+
+echo "Initializing container \${CONTAINER_NAME} from \${DOCKER_IMAGE}"
+\${DOCKER_CMD} run -itd --name \${CONTAINER_NAME} \\
+   -v \${SRC_DIR}:/home/devops/src \\
+   \${ENV_FILE} \\
+   -w /home/devops/src \\
+   \${DOCKER_IMAGE}
+
+echo "Attaching to running container..."
+\${DOCKER_CMD} attach \${CONTAINER_NAME}
+`
