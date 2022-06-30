7378a869673acfdd3b66036f72cbf7ebbad53ae0
Sean Sundberg <seansund@us.ibm.com>
2022-06-30 14:36:41 -0500

    Fixes install.sh script on Ubuntu
    - Changes to run in sh shell and fixes test syntax
    - Adds check for curl cli
    
    closes #157
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- install.sh
+++ install.sh
@@ -1,24 +1,29 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 DEST_DIR="${1:-$DEST_DIR}"
 RELEASE="${2:-$RELEASE}"
 
-if [[ -z "${DEST_DIR}" ]]; then
+if [ -z "${DEST_DIR}" ]; then
   DEST_DIR="/usr/local/bin"
 fi
 
-if [[ -z "${RELEASE}" ]]; then
-  RELEASE=$(curl -sL "https://api.github.com/repos/cloud-native-toolkit/iascable/releases/latest" | grep tag_name | sed -E 's/.*"tag_name": ?"([^"]+)".*/\1/g')
+if ! command -v curl 1> /dev/null 2> /dev/null; then
+  echo "curl cli not found" >&2
+  exit 1
+fi
+
+if [ -z "${RELEASE}" ]; then
+  RELEASE=$(curl -sL "https://api.github.com/repos/cloud-native-toolkit/iascable/releases/latest" | grep tag_name | sed 's/"tag_name"//g' | sed 's/ //g' | sed 's/://g' | sed 's/,//g' | sed 's/"//g')
 fi
 
 TYPE="linux"
 OS=$(uname)
-if [[ "$OS" == "Linux" ]]; then
+if [ "$OS" = "Linux" ]; then
   TYPE=$(cat /etc/os-release | grep -E "^ID=" | sed "s/ID=//g")
-  if [[ "${TYPE}" != "alpine" ]]; then
+  if [ "${TYPE}" != "alpine" ]; then
     TYPE="linux"
   fi
-elif [[ "$OS" == "Darwin" ]]; then
+elif [ "$OS" = "Darwin" ]; then
   TYPE="macos"
 else
   echo "OS not supported"
