91118487f10c30b0edf535ebe79af916c91843e8
Noé Samaille <noe.samaille@ibm.com>
2022-10-25 12:51:21 +0000

    Removes memory-stream which cause circular dep
    Signed-off-by: Noé Samaille <noe.samaille@ibm.com>

--- src/util/file-util.ts
+++ src/util/file-util.ts
@@ -1,10 +1,6 @@
 import {chmodSync, lstatSync, promises} from 'fs';
 import {join} from 'path';
 import superagent from 'superagent';
-// @ts-ignore
-import MemoryStream from 'memory-stream';
-
-import {isDefined} from './object-util';
 
 export async function chmodRecursive(root: string, mode: number) {
   chmodSync(root, mode)
@@ -18,26 +14,7 @@ export async function chmodRecursive(root: string, mode: number) {
 
 export async function loadFile(path: string): Promise<string> {
   if (/^http.*/.test(path)) {
-    return new Promise((resolve, reject) => {
-      const ws = new MemoryStream()
-
-      try {
-        ws.on('finish', () => {
-          resolve(ws.toString())
-        })
-
-        superagent
-          .get(path)
-          .pipe(ws)
-          .on('error', (e: Error) => {
-            reject(e)
-          })
-      } finally {
-        try { ws.close() } catch (err) {
-          // ignore
-        }
-      }
-    })
+    return superagent.get(path).then(resp => resp.text)
   }
 
   return promises.readFile(path.replace(/^file:/, ''))
